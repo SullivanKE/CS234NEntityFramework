17558c9ce02b62f509c91f28d69981575beb9ec2
SullivanKE <102761752+SullivanKE@users.noreply.github.com>
2022-11-17 15:04:11 -0800

    AppUser tests complete

--- CS234NEF-Tests/AppUserTests.cs
+++ CS234NEF-Tests/AppUserTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CS234NEntityFramework.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,70 @@ namespace CS234NEF_Tests
 {
     public class AppUserTests
     {
+        BitsContext dbContext;
+        AppUser? u;
+        List<AppUser>? appUsers;
+
         [SetUp]
         public void Setup()
         {
+
+            dbContext = new BitsContext();
+
+            u = new AppUser();
+            u.Name = "New User";
+
         }
 
         [Test]
-        public void Test1()
+        public void GetAllTest()
         {
-            Assert.Pass();
+            // Get all customers
+            appUsers = dbContext.AppUsers.OrderBy<AppUser, int>(a => a.AppUserId).ToList();
+
+            Assert.IsNotNull(appUsers);
+            Assert.IsNotEmpty(appUsers);
+            Assert.That(appUsers[0].Name, Is.EqualTo(u.Name));
+
+        }
+        [Test]
+        public void CreateTest()
+        {
+            dbContext.AppUsers.Add(u);
+            dbContext.SaveChanges();
+
+            AppUser? u2 = dbContext.AppUsers.FirstOrDefault(n => n.Name == "New User");
+            Assert.IsNotNull(u2);
+            Assert.That(u2.Name, Is.EqualTo(u.Name));
+
+        }
+
+        [Test]
+        public void DeleteTest()
+        {
+
+            appUsers = dbContext.AppUsers.OrderBy<AppUser, int>(a => a.AppUserId).ToList();
+            int index = appUsers[appUsers.Count - 1].AppUserId;
+
+            u = dbContext.AppUsers.Find(index);
+            dbContext.AppUsers.Remove(u);
+            dbContext.SaveChanges();
+            Assert.IsNull(dbContext.AppConfigs.Find(index));
+        }
+
+        [Test]
+        public void UpdateTest()
+        {
+            appUsers = dbContext.AppUsers.OrderBy<AppUser, int>(a => a.AppUserId).ToList();
+            int index = appUsers[appUsers.Count - 1].AppUserId;
+
+            u = dbContext.AppUsers.Find(index);
+            u.Name = "New New User";
+            dbContext.SaveChanges();
+
+            AppUser u2 = dbContext.AppUsers.Find(index);
+            Assert.That(u2.Name, Is.EqualTo(u.Name));
+
         }
     }
 }
