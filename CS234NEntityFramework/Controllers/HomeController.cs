e65e5ff373908fa9e23b5e3569f32a69f858a657
SullivanKE <102761752+SullivanKE@users.noreply.github.com>
2022-11-26 16:40:04 -0800

    UI with bootstrap

---
+++ CS234NEntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using CS234NEntityFramework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS234NEntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        BitsContext _context;
+        public HomeController(BitsContext c) 
+        {
+            _context = c;
+        }
+        public IActionResult Index()
+        {
+            ViewData["AppConfig"] = _context.AppConfigs.FirstOrDefault();
+            return View();
+        }
+    }
+}
---
+++ CS234NEntityFramework/Data/RoleNav.cs
@@ -0,0 +1,16 @@
+﻿using CS234NEntityFramework.Models;
+
+namespace CS234NEntityFramework.Data
+{
+    public static class RoleNav
+    {
+        public static List<string> GetRoleNav(string role)
+        {
+            List<string> nav = new List<string>();
+
+
+
+            return nav;
+        }
+    }
+}
